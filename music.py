9e7a50b0050c44565aed2318c18c29a81f62fb77
alstjd4517 <47757626+alstjd4517@users.noreply.github.com>
2019-02-25 19:33:18 +0900

    Update music.py

--- music.py
+++ music.py
@@ -11,7 +11,7 @@ def get_prefix(bot, msg):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
     # Notice how you can use spaces in prefixes. Try to keep them simple though.
-    prefixes = ['.']
+    prefixes = ['!']
 
     return commands.when_mentioned_or(*prefixes)(bot, msg)
 
@@ -265,5 +265,4 @@ async def volume(con, vol: float):
     if player_status[con.message.server.id] == True:
         servers_songs[con.message.server.id].volume = vol
 
-access_token=os.environ["BOT_TOKEN"]
-bot.run(BOT_TOKEN)
+bot.run(os.environ['BOT_TOKEN'])
